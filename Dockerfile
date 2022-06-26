854d0908c02559d08c3829b63ef87d4359801d41
MattiaColombari <mattia.colombari00@gmail.com>
2022-06-26 18:12:06 +0200

    "hello_world" flask application structure and commands to run and debug flask inside docker, the docker image doesn't have java installed.

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM ubuntu:latest
+
+EXPOSE 5000
+
+RUN mkdir /app
+WORKDIR /app
+
+RUN apt-get update
+RUN apt-get -y install python3
+RUN apt-get -y install python3-pip
+
+COPY requirements.txt /app
+RUN pip install -r requirements.txt
+
+CMD python3 app.py
--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+
+# "docker build -t web_app ."
+# "docker run -p 5000:5000 -v $(pwd):/app web_app"
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+  return render_template("user_login.html")
 
 
 if __name__ == '__main__':
-    app.run()
+  port = int(os.environ.get('PORT', 5000))
+  app.run(debug=True, host='0.0.0.0', port=port)
