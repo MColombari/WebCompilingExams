adb29416eb8fa83cef51a0bd0f0c16dfe39e31c1
MattiaColombari <mattia.colombari00@gmail.com>
2022-07-03 12:44:03 +0200

    Typo and warning fix.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # "docker build -t web_app ."
 # "docker run -p 5000:5000 -v $(pwd):/app web_app"
 
-from flask import Flask, render_template, redirect, url_for, request, flash
+from flask import Flask, render_template, redirect, url_for, flash
 import os
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -12,26 +12,30 @@ app = Flask(__name__)
 # Chiave di sicurezza, abbiamo messo una string di caratteri casuali (hex).
 app.config['SECRET_KEY'] = '9c986a8dac94804409f30ecf62c2ce22'
 
+
 class MyForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
 
+
 @app.route('/')
 def hello_world():
-  return render_template("hello_page.html")
+    return render_template("hello_page.html")
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = MyForm()
     if form.validate_on_submit():
         flash(f'Ciao "{form.name.data}"', 'success')
-        return redirect('/success')
+        return redirect(url_for('success'))
     return render_template('user_login.html', form=form)
+
 
 @app.route('/success')
 def success():
-  return render_template("user_login_succeded.html")
+    return render_template("user_login_succeded.html")
 
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
