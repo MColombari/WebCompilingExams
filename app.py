c8d3edf9ca6c8add392b3b266f3e80412f126fb1
MattiaColombari <mattia.colombari00@gmail.com>
2022-07-03 12:42:16 +0200

    Basic login structure.

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template
-import os
-
 # "docker build -t web_app ."
 # "docker run -p 5000:5000 -v $(pwd):/app web_app"
 
+from flask import Flask, render_template, redirect, url_for, request, flash
+import os
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
+
+# Chiave di sicurezza, abbiamo messo una string di caratteri casuali (hex).
+app.config['SECRET_KEY'] = '9c986a8dac94804409f30ecf62c2ce22'
+
+class MyForm(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
 
 @app.route('/')
 def hello_world():
-  return render_template("user_login.html")
+  return render_template("hello_page.html")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = MyForm()
+    if form.validate_on_submit():
+        flash(f'Ciao "{form.name.data}"', 'success')
+        return redirect('/success')
+    return render_template('user_login.html', form=form)
+
+@app.route('/success')
+def success():
+  return render_template("user_login_succeded.html")
 
 
 if __name__ == '__main__':
