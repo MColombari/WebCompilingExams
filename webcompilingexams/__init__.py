d986bc90ddb2a2cc3a0c5362d521c0dc36bd1616
MattiaColombari <mattia.colombari00@gmail.com>
2022-07-08 17:43:47 +0200

    More advanced login system, doesn't let user go to url without been logged in.

--- webcompilingexams/__init__.py
+++ webcompilingexams/__init__.py
@@ -8,5 +8,8 @@ app.config['SECRET_KEY'] = '9c986a8dac94804409f30ecf62c2ce22'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'  # Il database verrà creato nella directory dell'app.
 db = SQLAlchemy(app)  # Funziona come Room.
 login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # Pagina a cui vado nel caso in cui l'utente cerchi accedere a una pagina senza
+# aver fatto l'accesso.
+login_manager.login_message = 'Per eccedere a questa pagina è encessario prima loggarsi'  # Messaggio mostrato.
 
 from webcompilingexams import routes
--- webcompilingexams/routes.py
+++ webcompilingexams/routes.py
@@ -1,7 +1,8 @@
+from flask_login import login_user, current_user, logout_user, login_required
 from werkzeug.exceptions import HTTPException
 from webcompilingexams import app, db
 from webcompilingexams.form import LoginForm
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 
 from webcompilingexams.models import User
 
@@ -13,24 +14,39 @@ def hello_world():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        flash(f'Utente già loggato', 'success')
+        return redirect(url_for('success'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User(id=int(form.matricola.data), name=form.nome.data, surname=form.cognome.data,
                     email=form.email.data)
         db.session.add(user)
         db.session.commit()
-        flash(f'User registrato con successo\n{user}"', 'success')
+        login_user(user, True)
+        next_page = request.args.get('next')
+
+        flash(f'User inserito con successo\n{user}"', 'success')
+        if next_page:
+            return redirect(next_page)  # Vai alla pagina a cui è cerrcato di andare senza il login.
         return redirect(url_for('success'))
 
     return render_template('user_login.html', title='Login', form=form)
 
 
 @app.route('/success')
+@login_required
 def success():
     return render_template("user_login_succeded.html", title='Success')
 
 
+@app.route('/logout')
+def logout():
+    logout_user()
+    return render_template("hello_page.html", title='Logout')
+
+
 @app.errorhandler(HTTPException)
 def errorhandler(e):
-    print(e.code + e.name + e.description)
+    print(f"{e.code}, {e.name}, {e.description}")
     return e  # Da completare
