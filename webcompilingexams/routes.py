3b1a59b76247f1ca48685236e6afee5e1e08efa2
MattiaColombari <mattia.colombari00@gmail.com>
2022-07-12 12:23:18 +0200

    Implementation of a bottom information bar, and improvement of login confirm popup.

--- webcompilingexams/routes.py
+++ webcompilingexams/routes.py
@@ -6,16 +6,24 @@ from flask import render_template, redirect, url_for, flash, request
 
 from webcompilingexams.models import User
 
+from datetime import datetime
+
+
+DATE = str(datetime.today().strftime('%Y / %m / %d'))
+
 
 @app.route('/')
 def hello_world():
-    return render_template("hello_page.html", title='Home')
+    return render_template("hello_page.html", title='Home',
+                           bottom_bar_left=DATE,
+                           bottom_bar_center='Pagina di benvenuto',
+                           bottom_bar_right='Non ancora registrato')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        flash('Utente già loggato', 'success')
+        flash('Utente già registrato', 'success')
         return redirect(url_for('success'))
     form = LoginForm()
     if form.validate_on_submit():
@@ -28,22 +36,34 @@ def login():
 
         flash(f'User inserito con successo: "{user}"', 'success')
         if next_page:
-            return redirect(next_page)  # Vai alla pagina a cui è cerrcato di andare senza il login.
+            return redirect(next_page)  # Vai alla pagina a cui ha cercato di andare precedentemente senza il login.
         return redirect(url_for('success'))
 
-    return render_template('user_login.html', title='Login', form=form)
+    return render_template('user_login.html', title='Login', form=form,
+                           bottom_bar_left=DATE,
+                           bottom_bar_center='Registrazione',
+                           bottom_bar_right='Attesa registrazione'
+                           )
 
 
 @app.route('/success')
 @login_required
 def success():
-    return render_template("user_login_succeded.html", title='Success')
+    return render_template("user_login_succeded.html", title='Success',
+                           bottom_bar_left=DATE,
+                           bottom_bar_center='Successo',
+                           bottom_bar_right='Registrazione avvenuta con successo'
+                           )
 
 
 @app.route('/logout')
 def logout():
     logout_user()
-    return render_template("hello_page.html", title='Logout')
+    return render_template("hello_page.html", title='Logout',
+                           bottom_bar_left=DATE,
+                           bottom_bar_center='Pagina di benvenuto',
+                           bottom_bar_right='Utente disconnesso'
+                           )
 
 
 @app.errorhandler(HTTPException)
